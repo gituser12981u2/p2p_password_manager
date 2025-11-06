802c9774c0938acaecd6a296d805a87665338384
Alexander Curtis <alexcu@tutanota.com>
2025-11-06 16:29:35 +0000

    test: add benches, not sure if mimalloc secure is good though

--- crates/backend/src/lib.rs
+++ crates/backend/src/lib.rs
@@ -1,2 +1,21 @@
 pub mod node;
 pub mod pinset;
+
+
+
+
+#[cfg(any(feature = "mimalloc", feature = "mimalloc-secure"))]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[cfg(feature="jemallocator")]
+#[global_allocator]
+static GLOBAL:jemallocator::Jemalloc=jemallocator::Jemalloc;
+
+#[cfg(feature="scudo")]
+#[global_allocator]
+static GLOBAL:scudo::GlobalScudoAllocator=scudo::GlobalScudoAllocator;
+
+#[cfg(feature="snmalloc")]
+#[global_allocator]
+static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
---
+++ crates/backend/store_bench_results_plain_text.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+## quick dirty script to store all bench results as txt file instead of html (well, you still get html, shut up.)
+
+./run_all_benches.sh > plain_text_bench_results.txt
